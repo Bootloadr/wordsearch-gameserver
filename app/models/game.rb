eefcc1feb58d483ec7428090b15b0cf5e1a34d88
Bootloadr <abhinavpatel2010@gmail.com>
2016-10-14 04:18:19 +0530

    Added word verification on grid letters

--- app/models/game.rb
+++ app/models/game.rb
@@ -40,6 +40,7 @@ class Game < ApplicationRecord
     end
       
     #Word Cheking
+    board = REDIS.lrange("grid_#{game_id}", 0, -1 )
     temp = REDIS.srandmember(game_id) 
     turn = "Turn: #{REDIS.hget(temp,"name")}"
     REDIS.set("turn_#{game_id}",temp)
@@ -49,7 +50,7 @@ class Game < ApplicationRecord
       ActionCable.server.broadcast "game_#{game_id}", { msg: msg , turn: turn, award: award, gamestatus: REDIS.get("status_#{game_id}")}
       return
     end
-    if (REDIS.sismember("wordlist", data['word'].upcase))
+    if search(data['word'], board) and (REDIS.sismember("wordlist", data['word'].upcase))
         REDIS.sadd("foundwords_#{game_id}", data['word'].upcase)
         REDIS.hset(plr_id, "points", (REDIS.hget(plr_id, "points").to_i + 1).to_s)
         msg = "#{REDIS.hget(plr_id,"name")}, SUCCESS!"
@@ -61,4 +62,35 @@ class Game < ApplicationRecord
     REDIS.set("passcount_#{game_id}", 0)
     ActionCable.server.broadcast "game_#{game_id}", { msg: msg , turn: turn, award: award , gamestatus: REDIS.get("status_#{game_id}")}  
   end
+  
+  private 
+   # verify the words
+  def self.search(word, board)
+       
+       0.upto(14) do |y|
+         0.upto(14) do |x|
+           [-1, 0, 1].each do |dy|
+             [-1, 0, 1].each do |dx|
+               next if dx == 0 and dy == 0
+               if self.search_for(word.strip.upcase,board, x, y, dx, dy) 
+                  return true
+               end
+             end
+           end
+         end
+       end
+        return false
+     end
+
+     # search for word in board starting at position (x,y) and moving in
+     # direction (dx,dy). returns true if found, false if not found.
+     def self.search_for(word,board, x, y, dx, dy)
+       return false if x < 0                 or
+                       x >= 15 or
+                       y < 0                 or
+                       y >= 15
+       return false if board[15*x + y] != word[0]
+       return true if word.length <= 1
+       return search_for(word[1,word.length-1],board, x + dx, y + dy, dx, dy)
+     end
 end
